3d9a155dd29579f26f4a100b335258585c8c9fe1
Manish Basnet <mbasnet911@gmail.com>
2021-06-03 22:34:32 +0545

    change request done

--- src/views/MyCollection/components/NftProgressSimple.tsx
+++ src/views/MyCollection/components/NftProgressSimple.tsx
@@ -1,31 +1,22 @@
 import React, { useContext } from 'react'
 import { Card, CardBody, Text } from '@pancakeswap-libs/uikit'
 import useI18n from 'hooks/useI18n'
-import nfts from 'config/constants/nfts'
 import { NftProviderContext } from '../contexts/NftProvider'
 import InfoRow from './InfoRow'
 
 const NftProgressSimple = () => {
   const TranslateString = useI18n()
-  const { isInitialized, currentDistributedSupply, totalSupplyDistributed, countBurnt, myMints } = useContext(
+  const {  balanceOf } = useContext(
     NftProviderContext,
   )
-
-  let myNFTs = 0
-
-  nfts.forEach(function (nft, key) {
-    const { nftId } = nft
-    const MINTS = myMints[nftId] || 0
-    myNFTs += MINTS
-  })
 
   return (
     <Card>
       <CardBody>
         <InfoRow>
-          <Text>{TranslateString(999, 'My Base and Rare NFTs')}:</Text>
+          <Text>{TranslateString(999, 'My NFTs')}:</Text>
           <Text>
-            <strong>{myNFTs}</strong>
+            <strong>{balanceOf}</strong>
           </Text>
         </InfoRow>
       </CardBody>
