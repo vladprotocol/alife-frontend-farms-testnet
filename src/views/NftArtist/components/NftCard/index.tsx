660b1e8d6da35c648f3fa55af7e7a6a3d2d8c07f
shrijana <shrijana1991@gmail.com>
2021-07-15 23:08:02 +0545

    minor fix

--- src/views/NftArtist/components/NftCard/index.tsx
+++ src/views/NftArtist/components/NftCard/index.tsx
@@ -242,7 +242,7 @@ const NftCard: React.FC<NftCardProps> = ({ nft }) => {
               Sold Out
             </Tag>
           )}
-          {isInitialized && walletCanClaim && (
+          {isInitialized && walletOwnsNft && (
             <Tag outline variant="secondary">
               {TranslateString(999, 'In Wallet')}
             </Tag>
